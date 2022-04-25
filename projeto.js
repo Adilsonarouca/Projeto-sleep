1f05a643b46557cfd1ed64cf7009084bf7ae7f79
Adilsonarouca <adilsonarouca07@gmail.com>
2022-04-25 19:57:21 -0300

    Projeto sleep

---
+++ projeto.js
@@ -0,0 +1,43 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles= document.querySelectorAll('.circle')
+
+
+
+let currentActive = 1 
+
+
+next.addEventListener('click', () => {
+currentActive++
+
+if(currentActive > circles.length){
+    currentActive = circles.length
+}
+update()
+
+})
+
+
+next.addEventListener('click', () => {
+    currentActive--
+    
+    if(currentActive <1){
+        currentActive = 1
+    }
+    
+    update()
+    })
+
+    function update (){
+
+        circles.forEach ((circles), idx) => {
+            if (idx < currentActive) {
+                circle.classList.add ('active')
+             } else {
+                        circle.classList.remove('active')
+                    }
+                }
+            }
+        }
+    }
